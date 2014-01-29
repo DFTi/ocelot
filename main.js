47971865a6a281071c0162a8d6469d39ee04764e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-01-29 03:06:59 -0800

    More more

--- main.js
+++ main.js
@@ -2,15 +2,34 @@ global.$ = $;
 
 var fs = require('fs');
 
+var net = require('net');
+
+
 $(document).ready(function() {
-  $('#send button[type=submit]').click(function() {
-    var filepath = $('#send #file').val();
+  var status = $('#status'),
+  fileinput = $('#send #file');
+
+  status.text('starting up');
+  
+  var server = net.createServer(function(c) { //'connection' listener
+    console.log('server connected');
+    c.on('end', function() {
+      console.log('server disconnected');
+    });
+    c.write('hello\r\n');
+    c.pipe(c);
+  });
+
+  server.listen(8124, function() { //'listening' listener
+    status.prepend("test");
+  });
+
+  fileinput.change(function() {
+    var filepath = fileinput.val();
     fs.exists(filepath, function(exists) {
       if (exists) {
-        alert("queuing file "+filepath+" for clients that have this key");
-      } else {
-        alert('no such file');
+        alert(filepath);
       }
-    });
-  });  
+    });  
+  });
 });
