0e22dee66d4b558198d90647ec7b36784601477e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-01-30 20:38:08 -0800

    Better log behavior

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ var Ocelot = require("./lib/ocelot.js"), ocelot = new Ocelot();
 global.ocelot = ocelot;
 global.$ = $;
 
-// Add the route
+var scrollBottom = function(e) {
+  var height = e[0].scrollHeight;
+  e.scrollTop(height);
+};
 
 $(document).ready(function() {
   var ui = {
     tx: {
-      log: function(m) { $('.logs .tx pre').append(m+"\n") },
+      log: function(m) { scrollBottom($('.logs .tx pre').append(m+"\n")) },
       file: $('.tx input[name=file]')
     },
     rx: {
-      log: function(m) { $('.logs .rx pre').append(m+"\n") },
+      log: function(m) { scrollBottom($('.logs .rx pre').append(m+"\n")) },
       host: $('.rx input[name=host]')
     }
   };
