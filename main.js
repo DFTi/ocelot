ef7e80978e1f52772f72b54cfb794dd12f221545
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-01-31 01:39:28 -0800

    Allow changing the port

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var Ocelot = require("./lib/ocelot.js"), ocelot = new Ocelot();
 global.ocelot = ocelot;
 global.$ = $;
 
+process.env.PORT = 7777;
+
 var scrollBottom = function(e) {
   var height = e[0].scrollHeight;
   e.scrollTop(height);
@@ -11,7 +13,8 @@ $(document).ready(function() {
   var ui = {
     tx: {
       log: function(m) { scrollBottom($('.logs .tx pre').append(m+"\n")) },
-      file: $('.tx input[name=file]')
+      file: $('.tx input[name=file]'),
+      port: $('.tx input[name=port]')
     },
     rx: {
       log: function(m) { scrollBottom($('.logs .rx pre').append(m+"\n")) },
@@ -22,15 +25,32 @@ $(document).ready(function() {
 
   global.ui = ui;
 
-  ocelot.server.listen(7777, function () {
-    ui.tx.log('Listening on 7777');
-  });
 
   ui.tx.file.change(function(e) { ocelot.serve($(e.target).val()); });
   ui.rx.host.change(function(e) { ocelot.receive($(e.target).val()); });
+  ui.tx.port.change(function(e) {
+    var port = parseInt($(e.target).val());
+    var listen = function() {
+      ocelot.server.listen(port, function() {
+        ui.tx.log("Listening on port "+port);
+      });
+    };
+    if (port > 0) {
+      var addr = ocelot.server.address();
+      if (addr) {
+        if (addr.port !== port) {
+          ocelot.server.close(listen)
+        }
+      } else { 
+        listen();
+      }
+    }
+  });
   ui.rx.bin.change(function(e) {
     var input = $(e.target).val();
     ui.rx.log("Will try to use directory path "+input);
     ocelot.data.rx.binPath = input;
   });
+
+  ui.tx.port.val(process.env.PORT).change(); // start the server
 });
