8099dcf97ba92f7ee9e25da73fa017d1eb9fc04c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-01-31 08:28:26 -0800

    make it look good

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,76 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    clean: ["build"],
+    cssmin: {
+      combine: {
+        files: {
+          "build/css/app.css": "css/**/*.css"
+        }
+      }
+    },
+    copy: {
+      main: {
+        src: [
+          'node_modules/express/**/*',
+          'node_modules/request/**/*',
+          'node_modules/temp/**/*',
+          'node_modules/graceful-fs/**/*',
+          'node_modules/filed/**/*',
+          'fonts/**/*',
+          'images/**/*',
+          'package.json'
+        ],
+        dest: "build/"
+      },
+      wildcat: {
+        options: {
+          flatten: true
+        },
+        src: [
+          'src/ocelot/**/*.js'
+        ],
+        dest: 'build/'
+      }
+    },
+    jade: {
+      index: {
+        files: { "build/index.html": ['index.jade'] }
+      },
+      templates: {
+        options: {
+          client: true
+        },
+        files: {
+          "tmp/templates.js": ['templates/*.jade']
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+          'src/3rd_party/jquery-2.1.0.min.js',
+          'src/3rd_party/jquery.address.js',
+          'src/3rd_party/semantic.js',
+          'src/3rd_party/runtime.js',
+          'tmp/templates.js',
+          'src/main.js'
+        ],
+        dest: "build/js/app.js"
+      }
+    },
+  });
+
+  grunt.registerTask('default', [
+    'clean',
+    'cssmin',
+    'jade',
+    'concat',
+    'copy',
+  ]);
+ 
   grunt.registerTask("rand40", function() {
     var done = this.async();
     grunt.util.spawn({
@@ -9,4 +81,11 @@ module.exports = function(grunt) {
       done();
     });
   });
+
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-var Ocelot = require("./src/ocelot.js"), ocelot = new Ocelot();
+var Ocelot = require("./src/ocelot/ocelot.js"),
+ocelot = new Ocelot(),
+ui = {};
+
 global.ocelot = ocelot;
+global.ui = ui;
 global.$ = $;
 
 process.env.PORT = 7777;
@@ -9,25 +13,45 @@ var scrollBottom = function(e) {
   e.scrollTop(height);
 };
 
-$(document).ready(function() {
-  var ui = {
-    tx: {
-      log: function(m) { scrollBottom($('.logs .tx pre').append(m+"\n")) },
-      file: $('.tx input[name=file]'),
-      port: $('.tx input[name=port]')
-    },
-    rx: {
-      log: function(m) { scrollBottom($('.logs .rx pre').append(m+"\n")) },
-      host: $('.rx input[name=host]'),
-      bin: $('.rx input[name=bin]')
-    }
+var renderReceiver = function() {
+  $('#content').html(JST['templates/receiver']({
+    /* Saved data would go here */
+    queue: [],
+    settings: {}
+  }));
+
+  $('#menu .active').removeClass('active');
+  $('#menu #rx-tab').addClass('active');
+
+  ui.rx = {
+    log: function(m) { scrollBottom($('.logs .rx pre').append(m+"\n")) },
+    host: $('#content input[name=host]'),
+    bin: $('#content input[name=bin]')
   };
-
-  global.ui = ui;
-
-
-  ui.tx.file.change(function(e) { ocelot.serve($(e.target).val()); });
   ui.rx.host.change(function(e) { ocelot.receive($(e.target).val()); });
+  ui.rx.bin.change(function(e) {
+    var input = $(e.target).val();
+    ui.rx.log("Will try to use directory path "+input);
+    ocelot.data.rx.binPath = input;
+  });
+};
+
+var renderTransmitter = function() {
+  $('#content').html(JST['templates/transmitter']({
+    /* Saved data would go here */
+    receivers: [],
+    activity: []
+  }));
+
+  $('#menu .active').removeClass('active');
+  $('#menu #tx-tab').addClass('active');
+
+  ui.tx = {
+    log: function(m) { scrollBottom($('.logs .tx pre').append(m+"\n")) },
+    file: $('#content input[name=file]'),
+    port: $('#content input[name=port]')
+  };
+  ui.tx.file.change(function(e) { ocelot.serve($(e.target).val()); });
   ui.tx.port.change(function(e) {
     var port = parseInt($(e.target).val());
     var listen = function() {
@@ -46,11 +70,13 @@ $(document).ready(function() {
       }
     }
   });
-  ui.rx.bin.change(function(e) {
-    var input = $(e.target).val();
-    ui.rx.log("Will try to use directory path "+input);
-    ocelot.data.rx.binPath = input;
-  });
-
   ui.tx.port.val(process.env.PORT).change(); // start the server
+};
+
+$(document).ready(function() {
+  /* Setup the UI, render the receiver screen initially 
+   * Later, based on persisted settings we decide which view to render */
+  renderReceiver();
+  $("#menu #rx-tab").click(renderReceiver)
+  $("#menu #tx-tab").click(renderTransmitter);
 });
